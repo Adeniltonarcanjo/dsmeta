960e390b822003a86b71e8666a6d4bd7e547f865
Adeniltonarcanjo <Adeniltonmorais@gmail.com>
2022-07-21 23:02:35 -0300

    API test

---
+++ backend/src/main/java/com/github/adeniltonarcanjo/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.github.adeniltonarcanjo.dsmeta.repositories;
+
+import com.github.adeniltonarcanjo.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
